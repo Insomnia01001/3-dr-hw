d90d38b917542b928b9f0daf0478f847e1b67af2
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-09-17 22:41:58 +0300

    Add files via upload

---
+++ 3 dr hw/main.py
@@ -0,0 +1,14 @@
+son1= int(input("son kiriting"))
+son2= int(input("son kiriting"))
+c= son2 , son1
+print(f"{c}")
+s1=str(55)
+s2=int(88)
+s3=int(5.5)
+s4=float(102)
+print(type(s1))
+print(type(s2))
+print(type(s3))
+print(type(s4))
+
+
